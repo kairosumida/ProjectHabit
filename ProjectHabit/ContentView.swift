3e3044c9db50001bcdd70aa7567a4346ea6cd8a6
kairosumida <kairosumida@hotmail.com>
2022-05-06 16:25:00 -0300

    Initial Commit

---
+++ ProjectHabit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjectHabit
+//
+//  Created by Kairo Sumida on 06/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProjectHabit/ProjectHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectHabitApp.swift
+//  ProjectHabit
+//
+//  Created by Kairo Sumida on 06/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ProjectHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
