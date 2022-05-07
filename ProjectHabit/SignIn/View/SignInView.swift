788d24263a3688d398ca8c4ba12cfbb5d5ad25ea
kairosumida <kairosumida@hotmail.com>
2022-05-07 20:07:59 -0300

    Adicionado tela de login

--- ProjectHabit/SignIn/View/SignInView.swift
+++ ProjectHabit/SignIn/View/SignInView.swift
@@ -9,8 +9,89 @@ import SwiftUI
 
 struct SignInView: View{
     @ObservedObject var viewModel: SignInViewModel
+    
+    @State var email = ""
+    @State var password = ""
+    @State var action: Int? = 0
+    @State var navigationHidden = true
     var body: some View{
-        Text("Ola mundo")
-            .background(Color.blue)
+        NavigationView{
+            ScrollView(showsIndicators: false){
+                VStack(alignment: .center){
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.horizontal,48)
+
+                    Text("Login").foregroundColor(.orange)
+                        .font(Font.system(.title).bold())
+                        .padding(.bottom, 8)
+                    numberField
+                    passwordField
+                    enterButton
+                    registerLink
+                    Text("Copyright @YYY")
+                        .foregroundColor(Color.gray)
+                        .font(Font.system(size: 16).bold())
+                        .padding(.top, 16)
+                }
+            }
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding(.horizontal, 32)
+            .background(Color.white)
+            .navigationBarTitle("Login", displayMode: .inline)
+            .navigationBarHidden(navigationHidden)
+    }
+}
+extension SignInView{
+    var registerLink: some View{
+        VStack{
+            Spacer(minLength: 36)
+            VStack{
+                Text("Ainda não possui um login ativo?")
+                    .foregroundColor(.gray)
+                    .padding(.top, 48)
+                
+                ZStack{
+                    NavigationLink(
+                        destination: Text("Tela de Cadastro"),
+                        tag: 1,
+                        selection: $action,
+                        label:{EmptyView()})
+                    
+                    Button("Realize seu Cadastro"){
+                        self.action = 1
+                    }.padding(.top, 48)
+                }
+            }
+        }
+    }
+}
+
+extension SignInView{
+    var enterButton: some View{
+        Button("Entrar"){
+            
+        }
+    }
+}
+
+extension SignInView{
+    var passwordField: some View{
+        SecureField("", text: $password)
+            .border(Color.orange)
+    }
+}
+
+extension SignInView {
+    var numberField: some View{
+        TextField("", text: $email)
+            .border(Color.black)
+    }
+}
+struct SignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        let viewModel = SignInViewModel()
+        SignInView(viewModel: viewModel)
     }
 }
--- ProjectHabit/Splash/ViewModel/SplashViewModel.swift
+++ ProjectHabit/Splash/ViewModel/SplashViewModel.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 class SplashViewModel: ObservableObject{
-    @Published var uiState: SplashUIState = .goToSignInScreen
+    @Published var uiState: SplashUIState = .loading
     
     func onAppear(){
         //faz algo assincrono e muda o estado da uiState
         DispatchQueue.main.asyncAfter(deadline: .now()+3){
             //aqui é chamado depois de 3 seg
-            self.uiState = .goToSignInScreen
+            self.uiState = .error("erro na resposta do servidor")
         }
     }
 }
