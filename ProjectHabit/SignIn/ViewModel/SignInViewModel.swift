8cc3a50fc1d227a8b0f7d1f226ecc64926e01e79
kairosumida <kairosumida@hotmail.com>
2022-05-09 21:05:47 -0300

    Update SignInViewModel.swift

--- ProjectHabit/SignIn/ViewModel/SignInViewModel.swift
+++ ProjectHabit/SignIn/ViewModel/SignInViewModel.swift
@@ -16,7 +16,6 @@ class SignInViewModel: ObservableObject{
     
     init(){
         cancellable = publisher.sink{
-            value in print("Usuario criado! Go home: \(value)")
             value in
             
             if value {
